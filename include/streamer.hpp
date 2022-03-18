bb03abf6b84b4b2767f55dee6a4cc11218c35ee7
PhysicsX <ulasdikme@gmail.com>
2022-03-18 22:19:51 +0100

    Introduce streamer implementation

---
+++ include/streamer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/cudaobjdetect.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui_c.h>
+
+
+class Streamer
+{
+	public:
+		
+		Streamer(int, int, int);
+		
+
+		void returnFrame(cv::Mat&);
+
+		std::string pipeline;
+		int width;
+		int height;
+		int fps;
+		cv::VideoCapture cap;
+};
+
